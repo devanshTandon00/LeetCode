83a7856dee41cadf257dbd7f1046079d2150fbe1
devanshTandon00 <dtdansh@gmail.com>
2020-07-10 14:51:56 +0530

    Constantly committing

---
+++ Easy/DetectCycles/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode a = head;
+        ListNode b = head;
+
+        int flag = 0;
+        //Check for b.next handles the case where length 1
+        while(a != null && b != null && b.next != null){
+            a = a.next;
+            b = b.next.next;
+            if (a == b){
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 1){
+            return true;
+        }
+
+        return false;
+    }
+}
