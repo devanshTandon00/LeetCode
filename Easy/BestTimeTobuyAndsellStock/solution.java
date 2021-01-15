be6ed461a29f613c818654d7620ca33673ed030e
Devansh Tandon <devansh.tandon@sjsu.edu>
2021-01-15 16:48:29 +0530

    Leeting

--- Easy/BestTimeTobuyAndsellStock/solution.java
+++ Easy/BestTimeTobuyAndsellStock/solution.java
@@ -34,3 +34,20 @@ class Solution {
         return max - min;
     }
 }
+
+// All test cases passed
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < minPrice)
+                minPrice = prices[i];
+            else if(prices[i] - minPrice > maxProfit)
+                maxProfit = prices[i] - minPrice;
+        }
+
+        return maxProfit;
+    }
+}
