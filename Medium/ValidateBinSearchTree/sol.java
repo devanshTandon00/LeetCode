c0f1031fea50b74c454271b9cad4dc1bc6acb746
Devansh Tandon <devansh.tandon@sjsu.edu>
2021-01-15 15:32:07 +0530

    Leeting

---
+++ Medium/ValidateBinSearchTree/sol.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, null, null);
+    }
+
+    public boolean validate(TreeNode root, TreeNode left, TreeNode right){
+        if(root == null)
+            return true;
+
+        if(left != null && left.val >= root.val)
+            return false;
+
+        if(right != null && right.val <= root.val)
+            return false;
+
+        boolean l = validate(root.left, left, root);
+        boolean r = validate(root.right, root, right);
+
+        return l && r;
+    }
+}
