18c96d42fa5fede8953633b9bdc9df8eb3ffa536
Devansh Tandon <devansh.tandon@sjsu.edu>
2021-01-19 22:37:28 +0530

    leeting

--- Medium/TwoPointerContainerWithMostWater/solution.js
+++ Medium/TwoPointerContainerWithMostWater/solution.js
@@ -22,3 +22,32 @@ var maxArea = function(height) {
 
     return max
 };
+
+
+// optimized two pointer Solution
+// O(n) runtime and O(1) space
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let max = 0
+    let container = 0
+
+    let left = 0
+    let right = height.length - 1
+
+    while(left < right){
+        const x = right - left
+        container = x * Math.min(height[left], height[right])
+
+        max = Math.max(container, max)
+
+        if(height[left] < height[right])
+            left++
+        else
+            right--
+    }
+
+    return max
+};
